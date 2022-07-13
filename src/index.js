1d2321e5353574d1f4c836485a4f780cb6d9e13d
Iryna Bakhur <ira.bakhur@icloud.com>
2022-07-13 15:31:16 +0300

    refactor: use module.exports

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-const { Transform, pipeline } = require('stream');
+const { reverseStdin } = require('./modules/reverse');
 
-const transform = () => {
-  console.log(`Enter text to transform or press ctrl + c to exit`);
-  
-  const transformStream = new Transform({
-    transform (chunk, encoding, cb) {
-      try {
-        const chunkStringified = chunk.toString().trim();
-        const reversedString = chunkStringified.split('').reverse().join('');
-
-        cb(null, reversedString + `\n\n`);
-      } catch (error) {
-        cb(error);
-      }
-    }
-  });
-
-  pipeline(
-    process.stdin,
-    transformStream,
-    process.stdout,
-    (error) => {
-      console.error(error.message);
-    }
-  );
-};
-
-transform();
+reverseStdin();
