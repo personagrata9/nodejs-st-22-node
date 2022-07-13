d5fce837d54dd79485956ed1f8680c8966c860c8
Iryna Bakhur <ira.bakhur@icloud.com>
2022-07-13 15:26:15 +0300

    refactor: use module.exports

--- src/index.js
+++ src/index.js
@@ -1,38 +1,3 @@
-const { createReadStream, createWriteStream } = require('fs');
-const csv = require('csvtojson');
-const { pipeline } = require('stream');
-
-const convertCsvToJson = () => {
-  const sourcePath = 'src/csv/nodejs-hw1-ex1.csv';
-  const destinationPath = 'src/csv/nodejs-hw1-ex1.txt';
-  
-  const source = createReadStream(sourcePath);
-  const destination = createWriteStream(destinationPath);
-
-  const parserParameters = {
-    headers: ['book', 'author', 'amount', 'price'],
-    checkColumn: true,
-    colParser: {
-      'amount': 'omit',
-      'price': 'number',
-    },
-    checkType: true,
-  };
-
-  const converter = csv(parserParameters);
-
-  pipeline(
-    source,
-    converter,
-    destination,
-    (error) => {
-      if (error) {
-        console.error(error.message);
-      } else {
-        console.log('File was successfuly converted!');
-      }
-    }
-  );
-};
+const { convertCsvToJson } = require('./modules/convert');
 
 convertCsvToJson();
